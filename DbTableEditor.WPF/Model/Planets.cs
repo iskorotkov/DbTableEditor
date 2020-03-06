2fa71e8303153c6aba938c22a89a46f6b28e82bc
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-03-06 22:44:46 +0500

    Initial commit

---
+++ DbTableEditor.WPF/Model/Planets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbTableEditor.WPF
+{
+    public partial class Planets
+    {
+        public int Id { get; set; }
+        public int Size { get; set; }
+        public int Habitability { get; set; }
+        public int Population { get; set; }
+        public int? Approval { get; set; }
+        public int? EmpireId { get; set; }
+        public int StarId { get; set; }
+        public string Name { get; set; }
+
+        public virtual Empires Empire { get; set; }
+        public virtual Stars Star { get; set; }
+        public virtual Shipyards Shipyards { get; set; }
+    }
+}
