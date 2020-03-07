939fc36e3f19606c02294440946111197e00e9c1
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-03-07 13:32:18 +0500

    Implement editing of existing records

--- DbTableEditor.WPF/SpaceshipsWindow.xaml.cs
+++ DbTableEditor.WPF/SpaceshipsWindow.xaml.cs
@@ -1,15 +1,39 @@
 ﻿using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using DbTableEditor.WPF.Context;
+using DbTableEditor.WPF.Model;
 
 namespace DbTableEditor.WPF
 {
     public partial class SpaceshipsWindow : Window
     {
+        private bool _rowCommiting;
+
         public SpaceshipsWindow()
         {
             InitializeComponent();
             SetupGrid();
+
+            Grid.RowEditEnding += GridOnRowEditEnding;
+        }
+
+        private void GridOnRowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            if (_rowCommiting)
+            {
+                return;
+            }
+
+            var spaceship = (Spaceship)e.Row.Item;
+            using var context = new SpaceshipsContext();
+            context.Attach(spaceship);
+
+            _rowCommiting = true;
+            Grid.CommitEdit();
+            _rowCommiting = false;
+
+            context.SaveChanges();
         }
 
         private void SetupGrid()
