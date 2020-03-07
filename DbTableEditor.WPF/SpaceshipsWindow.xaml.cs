1f32a5cb36677207e762f447676570c0c4a157f3
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-03-07 12:31:18 +0500

    Implement fetching spaceships from DB

---
+++ DbTableEditor.WPF/SpaceshipsWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace DbTableEditor.WPF
+{
+    public partial class SpaceshipsWindow : Window
+    {
+        public SpaceshipsWindow()
+        {
+            InitializeComponent();
+            SetupGrid();
+        }
+
+        private void SetupGrid()
+        {
+            using var context = new SpaceshipsContext();
+            Grid.DataContext = context.Spaceships.ToList();
+        }
+    }
+}
