408355f0a4a4ae418c29818dc1a1ec5699c776df
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-03-08 10:05:45 +0500

    Implement adding new spaceships (fleet and shipyard ids are fake)

--- DbTableEditor.WPF/SpaceshipsWindow.xaml.cs
+++ DbTableEditor.WPF/SpaceshipsWindow.xaml.cs
@@ -18,6 +18,13 @@ namespace DbTableEditor.WPF
             Grid.RowEditEnding += GridOnRowEditEnding;
         }
 
+        private void CommitRowEdit()
+        {
+            _rowCommiting = true;
+            Grid.CommitEdit();
+            _rowCommiting = false;
+        }
+
         private void GridOnRowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
         {
             if (_rowCommiting)
@@ -27,19 +34,32 @@ namespace DbTableEditor.WPF
 
             var spaceship = (Spaceship)e.Row.Item;
             using var context = new SpaceshipsContext();
-            context.Attach(spaceship);
 
-            _rowCommiting = true;
-            Grid.CommitEdit();
-            _rowCommiting = false;
+            if (context.Spaceships.Any(s => s.Id == spaceship.Id))
+            {
+                // Updating
+                context.Attach(spaceship);
+            }
+            else
+            {
+                // Creating new
+                context.Spaceships.Add(spaceship);
 
+                // TODO: set real fleet and shipyard IDs
+                spaceship.FleetId = 1;
+                spaceship.ShipyardId = 1;
+            }
+
+            CommitRowEdit();
             context.SaveChanges();
         }
 
         private void SetupGrid()
         {
             using var context = new SpaceshipsContext();
-            Grid.DataContext = context.Spaceships.ToList();
+            Grid.DataContext = context.Spaceships
+                .OrderBy(s => s.Id)
+                .ToList();
         }
     }
 }
