38dd2ae041fc7e3e67028bf4484253bf0245b9f7
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-03-14 19:20:36 +0500

    Refactor InputSelectNumber and SpaceshipsTable

--- DbTableEditor.BlazorApp/Shared/InputSelectNumber.cs
+++ DbTableEditor.BlazorApp/Shared/InputSelectNumber.cs
@@ -1,31 +1,10 @@
 ﻿using System;
-using System.Globalization;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Rendering;
 
 namespace DbTableEditor.BlazorApp.Shared
 {
-    public class InputSelectNumber<TValue> : InputBase<TValue>
+    public class InputSelectNumber<TValue> : InputSelect<TValue>
     {
-        /// <summary>
-        /// Gets or sets the child content to be rendering inside the select element.
-        /// </summary>
-        [Parameter] public RenderFragment ChildContent { get; set; }
-
-        /// <inheritdoc />
-        protected override void BuildRenderTree(RenderTreeBuilder builder)
-        {
-            builder.OpenElement(0, "select");
-            builder.AddMultipleAttributes(1, AdditionalAttributes);
-            builder.AddAttribute(2, "class", CssClass);
-            builder.AddAttribute(3, "value", BindConverter.FormatValue(CurrentValueAsString));
-            builder.AddAttribute(4, "onchange", EventCallback.Factory.CreateBinder<string>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
-            builder.AddContent(5, ChildContent);
-            builder.CloseElement();
-        }
-
-        /// <inheritdoc />
         protected override bool TryParseValueFromString(string value, out TValue result, out string validationErrorMessage)
         {
             if (typeof(TValue) == typeof(int))
