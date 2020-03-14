2576e5af8ccef4d415eafb927a86b7aa8592dc56
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-03-14 13:18:09 +0500

    Add InputSelectNumber and dropdowns for shipyards and fleets

---
+++ DbTableEditor.BlazorApp/Shared/InputSelectNumber.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace DbTableEditor.BlazorApp.Shared
+{
+    public class InputSelectNumber<TValue> : InputBase<TValue>
+    {
+        /// <summary>
+        /// Gets or sets the child content to be rendering inside the select element.
+        /// </summary>
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        /// <inheritdoc />
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, "select");
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddAttribute(2, "class", CssClass);
+            builder.AddAttribute(3, "value", BindConverter.FormatValue(CurrentValueAsString));
+            builder.AddAttribute(4, "onchange", EventCallback.Factory.CreateBinder<string>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
+            builder.AddContent(5, ChildContent);
+            builder.CloseElement();
+        }
+
+        /// <inheritdoc />
+        protected override bool TryParseValueFromString(string value, out TValue result, out string validationErrorMessage)
+        {
+            if (typeof(TValue) == typeof(int))
+            {
+                if (int.TryParse(value, out var resultInt))
+                {
+                    result = (TValue)(object)resultInt;
+                    validationErrorMessage = null;
+                    return true;
+                }
+                else
+                {
+                    result = default;
+                    validationErrorMessage = "The chosen value is not a valid number.";
+                    return false;
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException($"{GetType()} does not support the type '{typeof(TValue)}'.");
+            }
+        }
+    }
+}
