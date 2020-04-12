7bee7a1724fe4f322d924a613094679b1845e2fc
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-04-12 18:58:40 +0500

    Add popup dialogs for editing roles and passwords

---
+++ DbTableEditor.BlazorApp/Model/PasswordChange.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DbTableEditor.BlazorApp.Model
+{
+    public class PasswordChange
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ DbTableEditor.BlazorApp/Model/RoleChange.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DbTableEditor.BlazorApp.Model
+{
+    public class RoleChange
+    {
+        [Required]
+        public string Role { get; set; }
+    }
+}
